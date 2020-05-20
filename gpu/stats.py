2b44c24174d5acc0b54f51eadb300f0991a00ab3
Jose Javier <josejavier.gonzalez.ortiz@gmail.com>
2020-05-20 12:09:22 -0400

    Summary functions from query JSON objects

---
+++ gpu/stats.py
@@ -0,0 +1,43 @@
+import pandas as pd
+
+def gpu_summary(query):
+    data = []
+    for i, gpu in enumerate(query):
+        data.append({'gpu': str(i), **gpu})
+        del data[-1]['processes']
+    return pd.DataFrame(data)
+
+def proc_summary(query):
+    # if isinstance(queries, list):
+    #     queries = {'localhost': queries}
+
+    data = []
+    for i, gpu in enumerate(query):
+        for p in gpu['processes']:
+            data.append({'gpu': str(i), **p})
+    return pd.DataFrame(data)
+
+
+
+def gpu_proc_summary(query):
+    gpus = gpu_summary(query)
+    procs = proc_summary(query)
+    if len(procs) == 0:
+        return gpus
+    return pd.merge(gpus, procs, how='left', on='gpu')
+
+def merge_summaries(summaries):
+    for host, summary in summaries.items():
+        summary['host'] = host
+    return pd.concat(summaries.values(), sort=False)
+
+def gpu_user_summary(queries):
+    # TODO implement for compatibility
+    g = df.groupby('userid')
+    mem = g['used_memory'].sum() # Total used memory
+    nprocs = g['pid'].count() # Total processes
+    g['host'].unique() # Unique hosts
+    df.groupby('userid')[['host','gpu']].nunique() # unique GPUS, (broken still)
+    df = pd.merge(mem, nprocs, on='userid')
+    df = df.sort_values(by=['used_memory', 'pid'], ascending=False)
+    pass
